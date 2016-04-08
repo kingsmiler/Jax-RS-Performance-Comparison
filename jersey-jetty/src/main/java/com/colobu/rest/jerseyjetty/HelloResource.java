5043692297820aadd443bb1792a428c01cf9dc8e
kingsmiler <kingsiler@163.com>
2016-04-08 16:16:34 +0800

    1)fallback jetty's version to 9.2, when using 9.3.8.v20160314, there is a nosuchmethodexception.
    2)modify jersy-jetty's accessed url to 127.0.0.1:8080/rest/hello, for uniform, before it's 127.0.0.1:8080/hello

--- jersey-jetty/src/main/java/com/colobu/rest/jerseyjetty/HelloResource.java
+++ jersey-jetty/src/main/java/com/colobu/rest/jerseyjetty/HelloResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 
-@Path("hello")
+@Path("/rest/hello")
 public class HelloResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
